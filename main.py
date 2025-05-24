fd566a799a048084713712d61157f0ba94d06814
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-05-23 22:07:17 -0600

    Add the player image to screen
    - The player image is now displayed onto the screen, but it currently
    cannot be controled with any key pressed.

--- main.py
+++ main.py
@@ -15,6 +15,29 @@ class Settings:
         self.BG_COLOR = (42, 135, 191)
 
 
+class Player(pygame.sprite.Sprite):
+    """A class to create and control the robot player."""
+
+    def __init__(self, game: "Platformer") -> None:
+        """Initialize the robot player and its position"""
+        super().__init__()
+
+        self.screen = game.screen
+        self.settings = game.settings
+        self.screen_rect = game.screen.get_rect()
+
+        # Load the robot image and get its rect
+        self.image = pygame.image.load(r"assets/robot.png").convert_alpha()
+        self.rect = self.image.get_rect()
+
+        # Start the player at the bottom-left of the screen
+        self.rect.bottomleft = self.screen_rect.bottomleft
+
+    def draw_me(self) -> None:
+        """Draw the robot at the current location."""
+        self.screen.blit(self.image, self.rect)
+
+
 class Platformer:
     """The main platformer class, use to create and run the game."""
 
@@ -24,10 +47,14 @@ class Platformer:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
+        # Creates the window and sets the caption
         self.screen = pygame.display.set_mode(
             (self.settings.SCREEN_WIDTH, self.settings.SCREEN_HEIGHT)
         )
         pygame.display.set_caption("Robot Platfomer")
+
+        # Creates the robot the player can control
+        self.player = Player(self)
 
     def run_game(self) -> None:
         """Starts the main loop of the game."""
@@ -46,6 +73,7 @@ class Platformer:
     def _update_screen(self) -> None:
         """Update all game elements and flip the screen."""
         self.screen.fill(self.settings.BG_COLOR)
+        self.player.draw_me()
         pygame.display.flip()
 
 
