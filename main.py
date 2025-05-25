7706e43f99c2908cf4bf87baaca02889998a7127
Cris <33710623+SpaghettiArchitect@users.noreply.github.com>
2025-05-25 01:03:46 -0600

    Add level creation funtionality
    - Now we can create a new level by defining a new level class with a
    unique level pattern.
    
    - The blocks of each level are defined in their own class.
    
    - The player currently cannot interact with the blocks of each level.

--- main.py
+++ main.py
@@ -10,12 +10,100 @@ class Settings:
         """Initialize the game settings."""
         # Screen settings
         self.SCREEN_WIDTH = 800
-        self.SCREEN_HEIGHT = 600
+        self.SCREEN_HEIGHT = 500
         self.FPS = 60
         self.BG_COLOR = (42, 135, 191)
 
         # Player's settings
         self.player_speed = 5
+
+        # Block's settings
+        self.BLOCK_SIZE = 50
+        self.BLOCK_COLOR = (18, 102, 79)
+
+
+class Block(pygame.sprite.Sprite):
+    """A class to define each block that makes the level."""
+
+    def __init__(self) -> None:
+        """Initializes a block of a fixed size."""
+        super().__init__()
+
+        self.settings = Settings()
+        self.color = self.settings.BLOCK_COLOR
+        self.size = self.settings.BLOCK_SIZE
+
+        self.image = pygame.Surface((self.size, self.size))
+        self.image.fill(self.color)
+
+        self.rect = self.image.get_rect()
+
+    def set_position(self, x: int, y: int) -> None:
+        """Updates the position of the block."""
+        self.rect.x = x
+        self.rect.y = y
+
+
+class Level:
+    """A generic super-class used to define a level."""
+
+    def __init__(self, game: "Platformer") -> None:
+        """Initializes all sprite groups of the level."""
+        self.screen = game.screen
+        self.settings = game.settings
+        self.block_list = pygame.sprite.Group()
+
+    def update(self) -> None:
+        """Update everything in this level."""
+        self.block_list.update()
+
+    def draw(self) -> None:
+        """Draw everything on this level."""
+
+        # Draw the background
+        self.screen.fill(self.settings.BG_COLOR)
+
+        # Draw all the sprites that we have
+        self.block_list.draw(self.screen)
+
+    def create(self, pattern: list[str]) -> None:
+        """Creates the structure of the level based on a string pattern."""
+        current_x = 0
+        current_y = self.settings.SCREEN_HEIGHT
+
+        for row in pattern[::-1]:
+            current_y -= self.settings.BLOCK_SIZE
+
+            if row == "":
+                continue
+
+            for block_type in row:
+                if block_type == "x":
+                    new_block = Block()
+                    new_block.set_position(current_x, current_y)
+                    self.block_list.add(new_block)
+
+                current_x += self.settings.BLOCK_SIZE
+
+            current_x = 0
+
+
+class Level_01(Level):
+    """A class that defines the layout of level 1."""
+
+    def __init__(self, game: "Platformer"):
+        """Creates level 1 of the game."""
+        super().__init__(game)
+
+        # The level layout
+        self.level = [
+            "___xxxx____xxxxx",
+            "",
+            "",
+            "xxxxxxxxxxxxxxxx",
+        ]
+
+        self.create(self.level)
 
 
 class Player(pygame.sprite.Sprite):
@@ -90,6 +178,14 @@ class Platformer:
         # Creates the robot the player can control
         self.player = Player(self)
 
+        # Create all the levels
+        self.level_list = []
+        self.level_list.append(Level_01(self))
+
+        # Set the current level
+        self.level_no = 0
+        self.current_level = self.level_list[self.level_no]
+
     def run_game(self) -> None:
         """Starts the main loop of the game."""
 
@@ -132,7 +228,9 @@ class Platformer:
 
     def _update_screen(self) -> None:
         """Update all game elements and flip the screen."""
-        self.screen.fill(self.settings.BG_COLOR)
+        # self.screen.fill(self.settings.BG_COLOR)
+        self.current_level.update()
+        self.current_level.draw()
         self.player.draw_me()
         pygame.display.flip()
 
